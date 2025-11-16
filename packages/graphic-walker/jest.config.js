e39096aca8bb293fefe809c40ac4b594330c30e1
Reza <mohammadreza.mirbastegan@gmail.com>
2025-11-16 01:43:04 -0800

    test(jest): ignore dist tests so jest runs src tests only

--- packages/graphic-walker/jest.config.js
+++ packages/graphic-walker/jest.config.js
@@ -5,4 +5,7 @@ module.exports = {
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
     },
+    // Only run tests from the TypeScript source and test folders; ignore dist artifacts which are ES modules
+    testMatch: ['**/src/**/*.test.[jt]s?(x)', '**/test/**/*.test.[jt]s?(x)'],
+    testPathIgnorePatterns: ['<rootDir>/dist/', '/node_modules/'],
 };
